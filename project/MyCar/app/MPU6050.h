ba062a411bf29b341efca53f28437b3f637150f5
assans <assans82@gmail.com>
2014-10-28 12:27:59 +0800

    读取mpu6050 的id成功

--- project/MyCar/app/MPU6050.h
+++ project/MyCar/app/MPU6050.h
@@ -1,7 +1,10 @@
 #ifndef _MPU6050_H_
 #define _MPU6050_H_
 #include "common.h"
-
+uint8 MPU6050_Init(void);
+void MPU6050_WriteReg(uint8 RegisterAddress, uint8 Data);
+uint8 MPU6050_ReadReg(uint8 RegisterAddress);
+int16 MPU6050_GetResult(uint8 Regs_Addr);
 
 
 
--- project/MyCar/app/MyCar.c
+++ project/MyCar/app/MyCar.c
@@ -3,24 +3,25 @@
 #include "Init.h"
 #include "Kalman.h"
 #include "angget.h"
-uint8 OUTPUT_DATA[] = { 0x03, 0xFC, 0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 0xFC,0x03 };
+#include "MPU6050.h"
+uint8 OUTPUT_DATA[] = { 0x03, 0xFC, 0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 0xFC,
+		0x03 };
 uint8 AllData[] = { 0x03, 0xFC, 0, 0, 0, 0, 1, 0xFC, 0x03 };
 uint8 ang, gyro, x, y, z;
+uint8 whoami=1;
 
 float CarAngle, CarAngSpeed, x_f;
 
 void main(void)
 {
 	CarInit();
-
+	whoami=MPU6050_Init();
 	while (1)
 	{
 		LPLD_GPIO_Toggle_b(PTA, 17);
 		LPLD_SYSTICK_DelayMs(50);
-
 	}
 }
-
 void PIT0_ISR(void)
 {
 	//test
